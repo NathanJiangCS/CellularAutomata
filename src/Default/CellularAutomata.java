28f22c249b67513007c52b30553cefdfb23429b5
edmond5050 <edmond_5050@hotmail.com>
2016-08-10 12:37:11 -0400

    Removed ColorRenderer and Jframe related code

--- src/Default/CellularAutomata.java
+++ src/Default/CellularAutomata.java
@@ -1,17 +1,8 @@
 package Default;
 
-import javax.swing.JFrame;
-
-import GUI.Grid;
-
 public class CellularAutomata {
 	public static void main(String[] args){
 		
-		Grid window = new Grid();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.setSize(600,600);
-		window.setVisible(true);
-
+	
 	}
 }
